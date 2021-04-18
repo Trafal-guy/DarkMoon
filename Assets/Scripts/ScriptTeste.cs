66456c34374ee753adad9808ab2c8e2671a4acd3
adrian hoinaski <adrianhoinaski@gmail.com>
2021-04-18 19:40:23 -0300

    Puzzle da caldeira (interruptores)

---
+++ Assets/Scripts/ScriptTeste.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Script para comandar os interruptores
+public class ScriptTeste : MonoBehaviour
+{
+    public int valorTotal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        valorTotal = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (valorTotal == 8)
+        {
+            print("deu");
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptTesteInterruptor.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Script para cada interruptor
+public class ScriptTesteInterruptor : MonoBehaviour
+{
+    public GameObject mainCamera;
+    public int valor;
+    public bool ativo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ativo = false;
+        if (tag == "Interruptor1")
+        {
+            valor = 1;
+        }
+        else if (tag == "Interruptor2")
+        {
+            valor = 2;
+        }
+        else if(tag == "Interruptor3")
+        {
+            valor = 3;
+        }
+        else if(tag == "Interruptor4")
+        {
+            valor = 4;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Triggered when you click on the object
+    private void OnMouseDown()
+    {
+        print(valor);
+        if (ativo == false)
+        {
+            mainCamera.GetComponent<ScriptTeste>().valorTotal += valor;
+            ativo = true;
+            print(mainCamera.GetComponent<ScriptTeste>().valorTotal);
+        }
+        else
+        {
+            mainCamera.GetComponent<ScriptTeste>().valorTotal -= valor;
+            ativo = false;
+            print(mainCamera.GetComponent<ScriptTeste>().valorTotal);
+        }
+    }
+}
